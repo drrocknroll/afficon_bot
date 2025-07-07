91e12cb39077348de7eca2c3a178087a0e8f335e
drrocknroll <aborkov@gmail.com>
2025-07-07 23:13:34 +0300

    Initial commit of AffiCon_Bot project

--- core/main.py
+++ core/main.py
@@ -1,8 +1,18 @@
-# core/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from aiogram import types
+
+# Импортируем бота и диспетчер из основного файла с логикой бота
+from core.agent import bot, dp
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello from afficon_bot!"}
+
+@app.post("/webhook")
+async def telegram_webhook(req: Request):
+    update = await req.json()
+    TelegramRequest = types.Update.to_object(update)
+    await dp.process_update(TelegramRequest)
+    return {"status": "ok"}
